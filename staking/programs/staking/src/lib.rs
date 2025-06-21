0f1112a701d48291c71ad373be12c9b28f7c7196
Absterrg0 <parvj5212@gmail.com>
2025-06-21 17:57:49 +0530

    contract works

--- staking/programs/staking/src/lib.rs
+++ staking/programs/staking/src/lib.rs
@@ -1,228 +1,271 @@
-use std::thread::current;
 
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{TransferChecked,transfer_checked};
-use anchor_lang::system_program::*;
+use anchor_spl::{
+    associated_token::AssociatedToken, token::{Mint, Token, TokenAccount},
+    token_interface::{transfer_checked,TransferChecked},
+    token::{mint_to,MintTo}
+};
 
 
-declare_id!("");
+
+declare_id!("GJRb1k6wCFTvMcNG9PA3mUQULZnqPU5GfrKNtm4H4SXx");
+
 
 #[error_code]
-pub enum StakeError {
-    #[msg("Amount must be greater than 0")]
-    InvalidAmount,
-    #[msg("Insufficient staked amount")]
-    InsufficientStake,
-    #[msg("Unauthorized access")]
-    Unauthorized,
-    #[msg("Arithmetic overflow")]
+pub enum CustomError{
+    #[msg("Insufficient Balance")]
+    InsufBalance,
+    #[msg("Overflow")]
     Overflow,
-    #[msg("Arithmetic underflow")]
-    Underflow,
-    #[msg("Invalid timestamp")]
-    InvalidTimestamp,
+    #[msg("Underflow")]
+    Underflow
 }
 
-const POINTS_PER_SOL_PER_DAY: u64 = 1_000_000; // Using micro-points for precision
-const LAMPORTS_PER_SOL: u64 = 1_000_000_000;
-const SECONDS_PER_DAY: u64 = 86_400;
-
-
-const POINTS_PER_LAMPORT_PER_SECOND: u128 = 
-    (POINTS_PER_SOL_PER_DAY as u128)
-    / (LAMPORTS_PER_SOL as u128)
-    / (SECONDS_PER_DAY as u128);
 
 #[program]
 pub mod staking{
+    use super::*;
+    pub fn initialise(ctx: Context<Initialise>)->Result<()>{
 
-
-    pub fn initialise_pda()->Result<()>{
-
+        let global_state = &mut ctx.accounts.global_state;
+        global_state.authority = ctx.accounts.signer.key();
+        global_state.bump = ctx.bumps.global_state;
+        global_state.reward_mint = ctx.accounts.reward_mint.key();
+        Ok(())
     }
 
-    pub fn stake()->Result<()>{
+    pub fn stake(ctx:Context<Stake>,amount:u64)->Result<()>{
 
-    }
+        let clock = Clock::get()?;
+        let user_stake = &mut ctx.accounts.user_stake_account;
 
-    pub fn unstake()->Result<()>{
+        user_stake.authority=ctx.accounts.signer.key();
+        user_stake.staking_mint = ctx.accounts.user_token_mint.key();
+        user_stake.staked_amount = user_stake.staked_amount.checked_add(amount).ok_or(CustomError::Overflow)?;
+        user_stake.start_slot = clock.slot;
+        user_stake.last_claimed_slot = clock.slot;
+
+
+        let transfer_accounts = TransferChecked{
+            from:ctx.accounts.user_token_ata.to_account_info(),
+            to: ctx.accounts.vault.to_account_info(),
+            authority:ctx.accounts.signer.to_account_info(),
+            mint : ctx.accounts.user_token_mint.to_account_info()
+        };
+
+        let cpi_context = CpiContext::new(ctx.accounts.token_program.to_account_info(), transfer_accounts);
         
+        transfer_checked(cpi_context, amount,ctx.accounts.user_token_mint.decimals)?;
+        Ok(())
     }
 
-    pub fn claim_points()->Result<()>{
+    pub fn unstake(ctx:Context<Unstake>)->Result<()>{
+
+        let user_stake = &mut ctx.accounts.user_stake_account;
+
+        let amount = user_stake.staked_amount;
+
+        let signer_key = ctx.accounts.signer.key();
+        let staking_mint_key = user_stake.staking_mint.key();
+        let bump_array = [ctx.bumps.vault];
+        let seeds = &[b"user_account", signer_key.as_ref(), staking_mint_key.as_ref(), &bump_array];
+
+        let transfer_accounts = TransferChecked{
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.user_token_ata.to_account_info(),
+            mint: ctx.accounts.user_token_mint.to_account_info(),
+            authority: ctx.accounts.vault_authority.to_account_info(),
+        };
+
+        let signer_seeds: &[&[&[u8]]] = &[seeds];
+        let cpi_context = CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), transfer_accounts, signer_seeds);
+
+
+        transfer_checked(cpi_context, amount, ctx.accounts.user_token_mint.decimals)?;
+
+
         
+        Ok(())
+    }
+    pub fn claim_rewards(ctx: Context<ClaimRewards>)->Result<()>{
+
+        let clock = Clock::get()?;
+
+        let user_stake = &mut ctx.accounts.user_stake_account;
+
+        let total_blocks = clock.slot - user_stake.last_claimed_slot;
+
+        let reward = total_blocks
+        .checked_mul(2)
+        .and_then(|v| v.checked_mul(user_stake.staked_amount))
+        .ok_or(CustomError::Overflow)?;
+
+        user_stake.last_claimed_slot = clock.slot;
+
+        let mint_accounts = MintTo{
+            to: ctx.accounts.user_reward_ata.to_account_info(),
+            mint:ctx.accounts.reward_mint.to_account_info(),
+            authority:ctx.accounts.reward_authority.to_account_info(),
+        };
+        let seeds: &[&[u8]] = &[b"mint_authority",&[ctx.bumps.reward_authority]];
+        let signer = &[seeds];
+        let cpi_context = CpiContext::new_with_signer(ctx.accounts.token_program.to_account_info(), mint_accounts,signer ); 
+        mint_to(cpi_context, reward)?;
+
+
+        Ok(())
     }
 }
 
 
 
-pub fn update_points(staking_pda_account: &mut StakeAccount, time: i64)->Result<()>{
-
-
-    let time_elapsed = time.checked_sub(staking_pda_account.last_updated_stake_time).ok_or(StakeError::InvalidTimestamp)?;
-
-    if time_elapsed>0 && staking_pda_account.stake_amount>0{
-        let new_points = calculate_points_earned(staking_pda_account.stake_amount, time_elapsed)?;
-        staking_pda_account.total_points = staking_pda_account.total_points.checked_add(new_points).ok_or(StakeError::Overflow)?;
-    }
-
-
-
-    Ok(())
-}
-
-
-
-pub fn calculate_points_earned(staked_amount: u64, time_elapsed: i64 )->Result<u64>{
-
-
-    let points = (staked_amount as u128).checked_mul(time_elapsed as u128).ok_or(StakeError::Overflow)?.checked_mul(POINTS_PER_LAMPORT_PER_SECOND).ok_or(StakeError::Overflow)?;
-
-    
-
-    Ok(points as u64)
-}
 
 
 #[account]
-pub struct StakeAccount{
-    pub owner: Pubkey,
-    pub stake_amount: u64,
-    pub last_updated_stake_time : i64,
-    pub bump: u8,
-    pub total_points: u64
+pub struct GlobalState{
+    authority: Pubkey,
+    reward_mint: Pubkey,
+    bump: u8,
+}
+
+
+
+#[account]
+pub struct UserStake{
+    pub authority:Pubkey,
+    pub staking_mint: Pubkey,
+    pub staked_amount: u64,
+    pub start_slot: u64,
+    pub last_claimed_slot: u64
 }
 
 
 
 
 #[derive(Accounts)]
-pub struct initialise_pda<'info>{
+pub struct Initialise<'info>{
     #[account(mut)]
-    owner: Signer<'info>,
-    pub system_program : Program<'info,System>,
-    #{account(
+    pub signer:Signer<'info>,
+    pub system_program:Program<'info,System>,
+    pub token_program: Program<'info,Token>,
+    #[account(
         init,
-        payer=owner,
-        space = 8+32+8+8+1+8,
+        payer=signer,
+        space = 32 + 32 + 1 + 8 + 1,
+        seeds=[b"state",signer.key().as_ref()],
+        bump
+    )]
+    pub global_state: Account<'info,GlobalState>,
+    pub reward_mint: Account<'info,Mint>
+}
+
+
+
+#[derive(Accounts)]
+pub struct Stake<'info>{
+
+    #[account(mut)]
+    pub signer:Signer<'info>,
+    pub system_program: Program<'info,System>,
+    pub user_token_mint: Account<'info,Mint>,
+    pub user_token_ata: Account<'info,TokenAccount>,
+    pub token_program: Program<'info,Token>,
+    #[account(
+        init_if_needed,
+        payer=signer,
+        token::mint = user_token_mint,
+        token::authority = vault_authority
+    )]
+    pub vault: Account<'info,TokenAccount>,
+    #[account(
+        seeds=[b"authority",user_token_mint.key().as_ref()],
+        bump
+    )]
+    ///CHECK: This is to just sign the transactions through the vault
+    pub vault_authority: UncheckedAccount<'info>,
+    #[account(
+        init_if_needed,
+        payer=signer,
+        space = 32 + 32 + 8 + 8 + 8 +8 + 1,
+        seeds = [b"user_account",signer.key().as_ref(),user_token_mint.key().as_ref()],
         bump,
-        seeds = [b"staking",owner.key.as_ref()]
-    )}
-    pub staking_pda_account: Account<'info,StakeAccount>
+    )]
+    pub user_stake_account: Account<'info,UserStake>,
 }
 
 
 
-impl<'info> initialise_pda<'info>{
 
-    pub fn create_pda(&self,init_amount:u64)->Result<()>{
-        let pda_account = self.staking_pda_account;
-        let clock = Clock::get()?;
-        pda_account.owner = self.owner.key;
-        if init_amount>0{
-            pda_account.stake_amount = init_amount;
-        }
-        else{
-            pda_account.stake_amount=0;
-        }
 
-        pda_account.last_updated_stake_time = clock.unix_timestamp;
-        pda_account.bump = self.bumps.staking_pda_account;
+#[derive(Accounts)]
+pub struct Unstake<'info>{
+
+    #[account(mut)]
+    signer:Signer<'info>,
+    pub system_program:Program<'info,System>,
+    pub token_program: Program<'info,Token>,
+    #[account(
+        seeds = [b"vault",user_stake_account.staking_mint.as_ref()],
+        bump
+    )]
+    pub vault: Account<'info,TokenAccount>,
+    /// CHECK: This is a PDA derived from [b"vault", mint]. It only signs, never read directly.
+    #[account(
+        seeds=[b"authority",user_stake_account.staking_mint.as_ref()],
+        bump
+    )]
+    pub vault_authority: UncheckedAccount<'info>,
+    #[account(
+        mut,
+        close = signer,
+        seeds=[b"user_account",signer.key().as_ref(),user_stake_account.staking_mint.as_ref()],
+        bump 
+    )]
+    pub user_stake_account: Account<'info,UserStake>,
+    #[account(
+        seeds=[b"state",signer.key().as_ref()],
+        bump = global_state.bump
+    )]
+    pub global_state: Account<'info,GlobalState>,
+    pub user_token_mint: Account<'info,Mint>,
+    pub user_token_ata: Account<'info,TokenAccount>,
+}
+
+
+
+#[derive(Accounts)]
+pub struct ClaimRewards<'info>{
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(
+        seeds=[b"user_account",signer.key().as_ref(),user_stake_account.staking_mint.as_ref()],
+        bump 
+    )]
+    pub user_stake_account: Account<'info,UserStake>,
+    #[account(
+        seeds = [b"state",signer.key().as_ref()],
+        bump
+    )]
+    pub global_state: Account<'info,GlobalState>,
+    #[account(mut)]
+    pub reward_mint: Account<'info,Mint>,
+    #[account(
         
-        Ok(())
-    }
-}
-
-
-
-#[derive(Accounts)]
-pub struct stake<'info>{
-    #[account(mut)]
-    owner: Signer<'info>,
-    #[account(
-        mut,
-        seeds = [b"staking",owner.key.as_ref()],
-        bump = staking_pda_account.bump
+        init_if_needed,
+        payer= signer,
+        associated_token::mint = reward_mint,
+        associated_token::authority = signer
     )]
-    pub staking_pda_account: Account<'info,StakeAccount>,
-    pub system_program: Program<'info,System>
-}
-
-
-impl<'info> stake<'info>{
-
-    pub fn stake(&mut self, amount:u64)-> Result<()>{
-
-            let pda_account = &mut self.staking_pda_account;
-            let clock = Clock::get()?;
-            update_points(pda_account, clock.unix_timestamp)?;
-
-            let transfer_accounts = Transfer{
-                from: self.owner.to_account_info(),
-                to: pda_account.to_account_info(),
-            };
-
-
-            let cpi_context = CpiContext::new(self.system_program.to_account_info(),transfer_accounts);
-
-
-            transfer(cpi_context, amount);
-
-            pda_account.stake_amount = pda_account.stake_amount.checked_add(amount).ok_or(StakeError::Overflow)?;
-
-        Ok(())
-    }
-}
-
-
-
-
-
-#[derive(Accounts)]
-pub struct unstake<'info>{
-    #[account(mut)]
-    owner: Signer<'info>,
+    pub user_reward_ata: Account<'info,TokenAccount>,
+    /// CHECK: This is a PDA derived from [b"vault", mint]. It only signs, never read directly.
     #[account(
-        mut,
-        seeds = [b"staking",owner.key.as_ref()],
-        bump = staking_pda_account.bump
+        seeds = [b"mint_authority"],
+        bump
     )]
-    pub staking_pda_account: Account<'info,StakeAccount>,
+    pub reward_authority:UncheckedAccount<'info>,
+    pub associated_token_program: Program<'info,AssociatedToken>,
+    pub token_program: Program<'info,Token>,
     pub system_program: Program<'info,System>
-}
 
-
-
-impl<'info> unstake<'info>{
-
-    pub fn unstake(&mut self, amount:u64)->Result<()>{
-
-        let staking_account = &mut self.staking_pda_account;
-        let clock = Clock::get()?;
-
-        update_points( staking_account, clock.unix_timestamp);
-
-        let owner_key = self.owner.key();
-        let seeds = &[
-            b"staking",
-            owner_key.as_ref(),
-            &[staking_account.bump]
-        ];
-        let signer = &[&seeds[..]];
-
-        let transfer_accounts = Transfer{
-            from:staking_account.to_account_info(),
-            to: self.owner.to_account_info(),
-
-        };
-
-        let cpi_context = CpiContext::new_with_signer(self.system_program.to_account_info(), transfer_accounts, signer);
-
-
-        transfer(cpi_context,amount);
-
-        staking_account.stake_amount = staking_account.stake_amount.checked_sub(amount).ok_or(StakeError::Underflow)?;
-
-        Ok(())
-    }   
 }
